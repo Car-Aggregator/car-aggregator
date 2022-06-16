2f4f930e40048857e7ac16d5cc3b65128d63f807
Maxwell Shick <maxwellshick@gmail.com>
2022-06-15 20:00:48 -0400

    Finished controller for saving new cars to favorites list

--- server/controllers/favoritesController.js
+++ server/controllers/favoritesController.js
@@ -3,22 +3,34 @@ const db = require('../models/pgModels.js');
 // Controller for validating credentials of our application's users
 const favoritesController = {};
 
-favoritesController.findUser = async (req, res, next) => {
-  const { email, password } = req.body;
+favoritesController.saveCar = async (req, res, next) => {
+  const { email, price, image, mileage, year, model, make, url, zip } = req.body;
+  // INSERT INTO favorited_cars (price, image, mileage, year, model, make, url, zip, user_id) VALUES ($2, $3, $4, $5, $6, $7, $8, $9)
+  // SELECT id FROM users WHERE email=email
 
   try {
-    const queryStr = 'SELECT * FROM users WHERE email=$1 AND password=$2'
-    const queryValues = [email, password];
-    const user = await db.query(queryStr, queryValues)
-    console.log('user-->', user)
-    res.locals.user = user.rows;
-    return next();
+    const findUser = 'SELECT id FROM users WHERE email=$1'
+    const emailValue = [email];
+    //REMOVE RETURNING AFTER DEBUGG FROM FRONTEND
+    const insertFav = 'INSERT INTO favorited_cars (price, image, mileage, year, model, make, url, zip, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *';
+    const favoritedValues = [price, image, mileage, year, model, make, url, zip]
+
+    await db.query(findUser, emailValue).then(user => {
+      const user_id = user.rows[0].id;
+      favoritedValues.push(user_id);
+      db.query(insertFav, favoritedValues).then(carInfo => {
+        //IF you want an array take out the [0]
+        res.locals.favoritedCar = carInfo[0].rows
+        return next();
+      })
+    });
   }
   catch (err) {
     return next({
-      log: 'Error in userController.findUser',
+      log: 'Error in favoritesController.saveCar',
       status: 502
     })
   }
 }
+
 module.exports = favoritesController;
--- server/routes/favoritesRoute.js
+++ server/routes/favoritesRoute.js
@@ -2,6 +2,11 @@ const express = require('express');
 const favoritesController = require('../controllers/favoritesController.js');
 const Router = express.Router();
 
-// Router.get()
+//Router.get()
+// '/favorites/
+Router.post('/addfavorite', favoritesController.saveCar, (req, res) => {
+  console.log('about to complete response to client from saveCar router');
+  return res.status(200).json(res.locals.favoritedCar);
+});
 
 module.exports = Router;
